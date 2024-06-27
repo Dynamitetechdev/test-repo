c3f942fbde7b936e0f0e58683df54cf03a16ba18
dynamitetechdev <oluwapoanu87@gmail.com>
2024-06-27 16:55:45 +0100

    0x: Update Config

--- config/index.jsx
+++ config/index.jsx
@@ -22,13 +22,9 @@ export const config = defaultWagmiConfig({
     projectId, // required
     metadata, // required
     ssr: true,
-    storage: createStorage({
-        storage: cookieStorage
-    }),
-    enableWalletConnect: true, // Optional - true by default
-    enableInjected: true, // Optional - true by default
-    enableEIP6963: true, // Optional - true by default
-    enableCoinbase: true, // Optional - true by default
+    // storage: createStorage({
+    //     storage: cookieStorage
+    // }),
     transports: {
         [mainnet.id]: http('https://eth-mainnet.g.alchemy.com/v2/cnF4veV0S7_dN_y0s3ygYlEE0ox-E_f5'),
         [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/Bzg0RGsBZsLA0r7hnsohUiuXl-8h2NgF'),
